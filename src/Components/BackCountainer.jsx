fba38bed1985ee8cccd11dd14a6c21275cb5de55
divshi <116270066+divshi@users.noreply.github.com>
2024-08-23 18:22:00 +0530

    second  commit

---
+++ src/Components/BackCountainer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import test1 from "../assets/img1.png";
+import test2 from "../assets/img2.png";
+import test3 from "../assets/img3.png";
+import test4 from "../assets/img4.png";
+
+const gradients = [
+  "bg-gradient-135",
+  "bg-gradient-225",
+  "bg-gradient-315",
+  "bg-gradient-45",
+];
+
+const images = [
+  `url(${test1})`,
+  `url(${test2})`,
+  `url(${test3})`,
+  `url(${test4})`,
+];
+
+function BackCountainer({ setBackground }) {
+  return (
+    <div className="w-3/4 mx-auto my-4">
+      {/* Render Gradients */}
+      <div className="flex flex-wrap justify-center items-center mb-4">
+        {gradients.map((item, index) => (
+          <div
+            key={index}
+            className={`w-24 h-12 mx-2 border border-gray-300 rounded-full shadow-2xl flex justify-center items-center cursor-pointer ${item}`}
+            onClick={() => setBackground(item)}
+          ></div>
+        ))}
+      </div>
+
+      {/* Render Images */}
+      <div className="flex flex-wrap justify-center items-center">
+        {images.map((item, index) => (
+          <div
+            key={index}
+            className="w-24 h-12 mx-2 border border-gray-300 rounded-full shadow-2xl flex justify-center items-center cursor-pointer"
+            style={{
+              backgroundImage: item,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            onClick={() => setBackground(item)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BackCountainer;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect } from "react";
+import imageEid from "../assets/eid.png";
+import imageEid2 from "../assets/eid (1).png";
+import imageEid3 from "../assets/eid (2).png";
+import imageEid4 from "../assets/eid (3).png";
+import html2canvas from "html2canvas";
+import BackCountainer from "./BackCountainer";
+
+const ImageContainer = ({ name, message, background, setBackground }) => {
+  return (
+    <div className="text-center flex-1">
+      <div
+        className={`shadow-md rounded flex flex-col my-2 flex-1 px-8 py-6 text-center h-96 items-center justify-center relative ${background.includes('bg-gradient') ? background : ''}`}
+        id="image-container"
+        style={background.includes('url') ? { backgroundImage: background, backgroundSize: 'cover', backgroundPosition: 'center' } : {}}
+      >
+        <img
+          src={imageEid}
+          alt="eid"
+          className="absolute top-0 w-12 sm:w-10 md:w-16 lg:w-20 xl:w-15 right-32 rotate-[25deg]"
+        />
+        <img
+          src={imageEid2}
+          alt="eid"
+          className="absolute w-100 h-100 top-0 right-0 rotate-[25deg] sm:rotate-[15deg]"
+        />
+        <img
+          src={imageEid3}
+          alt="eid"
+          className="absolute top-0 left-0 rotate-[-25deg]"
+        />
+        <img
+          src={imageEid4}
+          alt="eid"
+          className="absolute top-20 left-10 rotate-[-15deg] sm:rotate-[-25deg]"
+        />
+        <h1 className="text-2xl font-bold sm:text-3xl my-8 text-primary z-[2]">
+          Blessed Eid
+        </h1>
+        <p className="text-sm text-secondary text-right">
+          I wish my dear brother{" "}
+          <span className="text-primary font-bold FONT-ARABIC">{name}</span>{" "}
+          a blessed Eid and may you be in good health every year
+        </p>
+        <p className="text-sm text-secondary text-right mt-4">{message}</p>
+      </div>
+
+      <BackCountainer background={background} setBackground={setBackground} />
+    </div>
+  );
+};
+
+const FormContainer = ({
+  handleNameChange,
+  name,
+  handleMessageChange,
+  message,
+  handleGenerateImage,
+}) => {
+  return (
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 flex-1 mx-4">
+      <input
+        type="text"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-left placeholder-right mt-4 p-4"
+        placeholder="Enter your name"
+        onChange={handleNameChange}
+        value={name}
+      />
+      <textarea
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-left placeholder-right mt-4 h-40"
+        placeholder="Enter your message"
+        onChange={handleMessageChange}
+        value={message}
+      ></textarea>
+
+      <button
+        class="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring mt-4"
+        type="button"
+        onClick={handleGenerateImage}
+      >
+        Download the image<i class="ml-2 text-sm fas fa-download"></i>
+      </button>
+    </div>
+  );
+};
+
+function Home({ handlePartyModeClick }) {
+  const [name, setName] = React.useState("Name");
+  const [message, setMessage] = React.useState("");
+  const [background, setBackground] = React.useState("bg-gradient-135");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleGenerateImage = () => {
+    const element = document.getElementById("image-container");
+    html2canvas(element).then((canvas) => {
+      const link = document.createElement("a");
+      link.download = "image.png";
+      link.href = canvas.toDataURL("image/png");
+      link.click();
+    });
+
+    handlePartyModeClick();
+  };
+
+  return (
+    <div class="flex justify-center items-center h-screen flex-wrap  min-h-screen min-w-0 mx-10 px-10 ">
+      <ImageContainer
+        background={background}
+        name={name}
+        message={message}
+        setBackground={setBackground}
+      />
+      <FormContainer
+        handleNameChange={handleNameChange}
+        handleMessageChange={handleMessageChange}
+        name={name}
+        message={message}
+        handleGenerateImage={handleGenerateImage}
+      />
+    </div>
+  );
+}
+
+export default Home;
